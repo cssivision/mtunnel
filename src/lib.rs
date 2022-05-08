6d15fc628622d043e156e2414f2881c9a0402a1b
cssivision <cssivision@gmail.com>
2022-05-08 01:37:22 +0000

    add idletimeout

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+use std::future::Future;
 use std::io;
+use std::ops::{Add, Sub};
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
+use pin_project_lite::pin_project;
 use tokio::io::copy_bidirectional;
 use tokio::net::TcpStream;
+use tokio::time::{sleep, Duration, Instant, Sleep};
 
 macro_rules! ready {
     ($e:expr $(,)?) => {
@@ -20,19 +26,79 @@ mod stream;
 pub use stream::Stream;
 
 pub const ALPN_HTTP2: &[u8] = b"h2";
+const DEFAULT_VISITED_GAP: Duration = Duration::from_secs(3);
+const DEFAULT_IDLE_TIMEOUT: Duration = Duration::from_secs(5 * 60);
 
 pub fn other(desc: &str) -> io::Error {
     io::Error::new(io::ErrorKind::Other, desc)
 }
 
 pub async fn proxy(mut socket: TcpStream, mut stream: Stream) {
-    match copy_bidirectional(&mut socket, &mut stream).await {
-        Ok((n1, n2)) => {
-            log::debug!("proxy local => remote: {}, remote => local: {}", n1, n2);
+    match IdleTimeout::new(
+        copy_bidirectional(&mut socket, &mut stream),
+        DEFAULT_IDLE_TIMEOUT,
+    )
+    .await
+    {
+        Ok(v) => match v {
+            Ok((n1, n2)) => {
+                log::debug!("proxy local => remote: {}, remote => local: {}", n1, n2);
+            }
+            Err(e) => {
+                log::error!("copy_bidirectional err: {:?}", e);
+                stream.send_reset(h2::Reason::CANCEL);
+            }
+        },
+        Err(_) => {
+            log::error!("copy_bidirectional idle timeout");
         }
-        Err(e) => {
-            log::error!("copy_bidirectional err: {:?}", e);
-            stream.send_reset(h2::Reason::CANCEL);
+    }
+}
+
+pin_project! {
+    /// A future with timeout time set
+    pub struct IdleTimeout<S: Future> {
+        #[pin]
+        inner: S,
+        #[pin]
+        sleep: Sleep,
+        idle_timeout: Duration,
+        last_visited: Instant,
+    }
+}
+
+impl<S: Future> IdleTimeout<S> {
+    pub fn new(inner: S, idle_timeout: Duration) -> Self {
+        let sleep = sleep(idle_timeout);
+
+        Self {
+            inner,
+            sleep,
+            idle_timeout,
+            last_visited: Instant::now(),
+        }
+    }
+}
+
+impl<S: Future> Future for IdleTimeout<S> {
+    type Output = io::Result<S::Output>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut this = self.project();
+
+        match this.inner.poll(cx) {
+            Poll::Ready(v) => Poll::Ready(Ok(v)),
+            Poll::Pending => match Pin::new(&mut this.sleep).poll(cx) {
+                Poll::Ready(_) => Poll::Ready(Err(io::ErrorKind::TimedOut.into())),
+                Poll::Pending => {
+                    let now = Instant::now();
+                    if now.sub(*this.last_visited) >= DEFAULT_VISITED_GAP {
+                        *this.last_visited = now;
+                        this.sleep.reset(now.add(*this.idle_timeout));
+                    }
+                    Poll::Pending
+                }
+            },
         }
     }
 }
